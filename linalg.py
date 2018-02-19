039476a50d784fbb8c72ca99b49c2c8a9196a0ff
Andrew Zimolzak <andyzimolzak@gmail.com>
2018-02-19 11:14:31 -0500

    Pull in argv string. Aspect ratio square.

--- linalg.py
+++ linalg.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 import sys
 from numpy import pi, sin, cos, matrix, concatenate
 
+m_string = '1 0.5; 2 1.2'
+if len(sys.argv) > 1:
+    m_string = sys.argv[1]
+
 vi = sys.version_info
 assert vi.major == 2
 
@@ -18,10 +22,12 @@ for offset in [0, pi/2, pi, 1.5*pi]:
         unit_circle.append([sin(theta), cos(theta)])
 
 u = matrix(unit_circle)
-a = matrix('1 0; 0 2')
+a = matrix(m_string)
 p = u * a
 
 fig, ax = plt.subplots()
 ax.plot(u.T[0], u.T[1], 'bo')
 ax.plot(p.T[0], p.T[1], 'ro')
+ax.set_aspect(1.0)
+ax.set_title(str(a))
 plt.show()
