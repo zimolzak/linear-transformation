5273a36cce7b57bd4437611f9cc2cc5e858ffa62
Andrew Zimolzak <andyzimolzak@gmail.com>
2018-02-19 12:22:31 -0500

    Try to fix bug with eigenvecs as row vs column.

--- linalg.py
+++ linalg.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import sys
 from numpy import pi, sin, cos, matrix, concatenate, linalg
 
+## set up some globals
+
 m_string = '1 0.5; 2 1.2'
 if len(sys.argv) > 1:
     m_string = sys.argv[1]
@@ -16,16 +18,21 @@ vi = sys.version_info
 assert vi.major == 2
 
 unit_circle = []
-for offset in [0, pi/2, pi, 1.5*pi]:
-    for a in [0, pi/6, pi/4, pi/3]:
-        theta = offset + a
-        unit_circle.append([sin(theta), cos(theta)])
+theta = 0
+while theta <= 2 * pi:
+    unit_circle.append([sin(theta), cos(theta)])
+    theta += 2 * pi / 32
 
 u = matrix(unit_circle)
 a = matrix(m_string)
+
+## do calculations
+
 p = u * a
 evev = linalg.eig(a)
-eigenvecs = evev[1]
+eigenvecs = evev[1].T # default is return vecs in columns, so I transpose it.
+
+## plot unit circle and its transformation
 
 fig, ax = plt.subplots()
 
@@ -34,10 +41,16 @@ for i in range(len(u)):
     ys = [u[i,1], p[i,1]]
     plt.plot(xs, ys, 'go-')
 
+## plot eigenvectors
+
+print eigenvecs
+
 for i in range(len(eigenvecs)):
     xs = [0, eigenvecs[i,0]]
     ys = [0, eigenvecs[i,1]]
     plt.plot(xs, ys, 'r+-')
+
+## wrap up
 
 ax.set_aspect(1.0)
 ax.set_title(str(a))
